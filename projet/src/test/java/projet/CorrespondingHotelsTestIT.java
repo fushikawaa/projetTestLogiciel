1c64317a524846f051a590183be6f21ef666ecfc
lperrin011 <lperrin011@bordeaux-inp.fr>
2025-01-10 14:58:53 +0100

    test integration Hotel

---
+++ projet/src/test/java/projet/CorrespondingHotelsTestIT.java
@@ -0,0 +1,32 @@
+package projet;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CorrespondingHotelsTestIT {
+    @Test
+    public void testGetAllHotels(){
+        FileManager fileManager = new FileManager();
+        CorrespondingHotels correspondingHotels = new CorrespondingHotels("src/data/hotels_database.json", fileManager);
+        
+        correspondingHotels.getAllHotels();
+
+        List<Hotel> allHotels = correspondingHotels.getCorrespondingHotels();
+        
+        assertEquals(8, allHotels.size());
+    }
+    
+    @Test
+    public void testGetAllHotelsEmpty(){
+        FileManager fileManager = new FileManager();
+        CorrespondingHotels correspondingHotels = new CorrespondingHotels("src/data/empty_file.json", fileManager);
+        
+        correspondingHotels.getAllHotels();
+
+        List<Hotel> allHotels = correspondingHotels.getCorrespondingHotels();
+        
+        assertEquals(0, allHotels.size());
+    }
+}
